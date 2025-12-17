65200eaae01c874f4f70394a8e8c1f29f18becf9
MaciekAza <azarewiczmaciek@gmail.com>
2025-12-17 19:04:15 +0100

    szybka naprawa errorów

--- texter/src/context/UserProvider.jsx
+++ texter/src/context/UserProvider.jsx
@@ -1,22 +1,18 @@
 import { useLocalStorage } from '../hooks/useLocalStorage';
-import React, { useState, useEffect } from 'react';
-import { UserContext } from './userContext';
-
+import React from 'react';
+import { UserContext } from './UserContext';
 
 export function UserProvider({ children }) {
   const [userName, setUserName] = useLocalStorage('chat_username', '');
   const [status, setStatus] = useLocalStorage('chat_status', 'Dostępny');
 
-  useEffect(() => {
-    if (userName && userName.trim()) {
-      localStorage.setItem('chat_username', userName);
-    } else {
-      localStorage.removeItem('chat_username');
-    }
-  }, [userName]);
-
   return (
-    <UserContext.Provider value={{ userName, setUserName }}>
+    <UserContext.Provider value={{ 
+      userName, 
+      setUserName, 
+      status, 
+      setStatus 
+    }}>
       {children}
     </UserContext.Provider>
   );
